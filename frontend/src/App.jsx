09a97bacec061a5e3b709286ba98c483b79c9415
Roshan Reji <roshanreji987@gmail.com>
2024-12-13 02:53:12 +0530

    navbar component created with some addons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointment'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-
+      <Navbar />
+        <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        </Routes>
     </div>
   )
 }
